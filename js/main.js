2e7d0c636ab4c9cde2e5bff8f8b86211c2c04caf
FabioMau <maulucci.fabio@gmail.com>
2019-03-29 18:17:56 +0100

    AA

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+
+    var solid;
+    var pages = [
+        {
+            'Name': "Tetraedro",
+            'id': "tetraedro",
+            'linkid': "linkTetraedro",
+            'render': function () {
+                return new Solid(new THREE.TetrahedronGeometry(100, 0)).html();
+            }
+        }, {
+            'Name': "Esaedro",
+            'id': "esaedro",
+            'linkid': "linkEsaedro",
+            'render': function () {
+                return new Solid(new THREE.CubeGeometry(100, 100, 100));
+            }
+        }, {
+            'Name': "Ottaedro",
+            'id': "ottaedro",
+            'linkid': "linkOttaedro",
+            'render': function () {
+                return new Solid(new THREE.OctahedronGeometry(100, 0));
+            }
+        }, {
+            'Name': "Dodecaedro",
+            'id': "dodecaedro",
+            'linkid': "linkDodecaedro",
+            'render': function () {
+                return new Solid(new THREE.IcosahedronGeometry(100, 0));
+            }
+        }, {
+            'Name': "Icosaedro",
+            'id': "icosaedro",
+            'linkid': "linkIcosaedro",
+            'render': function () {
+                return new Solid(new THREE.DodecahedronGeometry(100, 0));
+            }
+        }
+    ];
+
+    setPage(0);
+
+    $(document)
+        .on('click', '.solidlink', function () {
+            pos = 0;
+            pages.forEach((page, index) => {
+                if (page.linkid == $(this).attr('id'))
+                    pos = index;
+            });
+            setPage(pos);
+        });
+
+    function setPage(pos) {
+        //solid.detroy();
+        pages.forEach(page => {
+            $('#' + page.id).hide();
+            $('#' + page.linkid).parent().removeClass('active');
+            $('.solido').empty();
+        });
+        $('#' + pages[pos].id).show();
+        $('#' + pages[pos].linkid).parent().addClass('active');
+        solid = pages[pos].render();
+        $('#' + pages[pos].id + ' .solido').append(solid.html());
+    }
+
+});
---
+++ js/solid.js
@@ -0,0 +1,62 @@
+class Solid {
+
+
+    constructor(Geometry) {
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.scene = new THREE.Scene();
+
+        this.scene.add(this.camera);
+        this.scene.background = new THREE.Color(0xffffff);
+        this.renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+
+        this.camera.position.z = 200;
+
+        // Material
+        this.SolidMat = new THREE.MeshPhongMaterial({
+            color: new THREE.Color("rgb(0,228,190)"),
+            emissive: new THREE.Color("rgb(0,0,0)"),
+            specular: new THREE.Color("rgb(0,228,190)")
+        });
+        this.LineMat = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 4 });
+
+        this.L1 = new THREE.PointLight(0xDDDDDD, 1);
+        this.L1.position.z = 100;
+        this.L1.position.y = 100;
+        this.L1.position.x = 100;
+        this.scene.add(this.L1);
+
+        this.L2 = new THREE.PointLight(0xDDDDDD, 1);
+        this.L2.position.z = 200;
+        this.L2.position.y = 50;
+        this.L2.position.x = -100;
+        this.scene.add(this.L2);
+
+        this.object = new THREE.Mesh(Geometry, this.SolidMat);
+        this.object.rotation.z = 0.1;
+        this.scene.add(this.object);
+
+        this.geo = new THREE.EdgesGeometry(this.object.geometry);
+        this.wireframe = new THREE.LineSegments(this.geo, this.LineMat);
+        this.wireframe.renderOrder = 1;
+        this.object.add(this.wireframe);
+    }
+
+    animation()  {
+        this.object.rotation.x += 2 / 100;
+        this.object.rotation.y += 2 / 100;
+        this.renderer.render( this.scene, this.camera );
+    
+    }
+
+    render() {
+        setTimeout(() => {
+            this.animation();
+        }, 10);
+    }
+
+    html() {
+        return this.renderer.domElement;
+    }
+
+}
